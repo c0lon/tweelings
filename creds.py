01861e6324969630bb681be5e3e10afc2fb8d836
Collin Barth <cpbarth92@gmail.com>
2015-02-16 00:25:17 -0500

    updated credentials

--- creds.py
+++ creds.py
@@ -3,5 +3,5 @@ class Creds:
         self.c_key = "V7Nw0tT3dps5qlo7ICoWE1uI6"
         self.c_secret = "NHEVWdnpsagNvlpgWITdMCvmoXGSn6Hfkb2hYnfBXDH2laG9xd"
 
-        self.a_token = "613854165-7EtR15BOUXWIiEyXj7eYzI6BWxBIwcOJZhheDyYO"
-        self.a_secret = "HQfz4oruzSOojay0LYM5lZQpmRCTQQMZwsbup8uwyXdhQ"
+        self.a_token = "613854165-OqM0QjS2rJsSA2Y0Cq1hnnp0BQ1FPQ7HPrKlEEEZ"
+        self.a_secret = "POi1vDKYE5z89IaZ1K4EjBBEJc5N9gR26wjkTcYi5B7VY"
