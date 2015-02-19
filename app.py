e849400996785a6b8de1c426af08b3720d890b08
Collin Barth <cpbarth92@gmail.com>
2015-02-19 17:20:24 -0500

    find most active time of users

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 """
 TODO
 
-handle everything that has to do with all tweets in one pass
-make sure mostRetweeted tweet is not itself a retweet
+time zones?
+links ratio
+
+keep frequencies with 'most' statistics
 """
 
 import json
+import time
 import tweepy
 from tweepy.error import TweepError
 from creds import Creds
 from user import User
 from sys import argv, exit
+
+DATEFORMAT = "%Y-%m-%d %H:%M:%S"
 
 # login to the twitter API using the Creds class
 def login():
@@ -77,12 +82,11 @@ def analyzeUser(user):
                     'name' : user.name,
                     'screen_name' : user.screen_name,
                     'friends' : user.friends_count,
-                    'followers' : user.followers_count,
-                    'tweets_retrieved' : len(tweets)}
+                    'followers' : user.followers_count}
         userJSON['user'] = userInfo
 
         # dict to hold all analysis info
-        analysisInfo = {}
+        analysisInfo = {'tweets_retrieved' : len(tweets)}
 
         # most favorited tweet
         mostFavorited = tweets[0]
@@ -96,6 +100,13 @@ def analyzeUser(user):
 
         # dict of words/frequencies
         wordFreqs = {}
+
+        # dict of active times
+        timeFreqs = {'early_morning' : 0, #  4:00AM -  6:59AM
+                     'morning'       : 0, #  7:00AM - 11:59AM
+                     'afternoon'     : 0, # 12:00PM -  4:59PM
+                     'evening'       : 0, #  7:00PM - 11:59PM
+                     'late_night'    : 0} # 12:00AM -  3:59AM
 
         for tweet in tweets:
             # split the tweet into words
@@ -113,30 +124,40 @@ def analyzeUser(user):
                         else:
                             wordFreqs[lower] += 1
 
-                # find most favorited tweet
-                if tweet.favorite_count > mostFavorited.favorite_count:
-                    mostFavorited = tweet
-
-                # find most retweeted tweet
+                # find most retweeted original tweet
                 if tweet.retweet_count > mostRetweets:
                     mostRetweeted = tweet
                     mostRetweets = tweet.retweet_count
 
-                # find the time of day the user is most active
+            # find most favorited tweet
+            if tweet.favorite_count > mostFavorited.favorite_count:
+                mostFavorited = tweet
+
+            # find the time of day the user is most active
+            hour = time.strptime(str(tweet.created_at), DATEFORMAT).tm_hour
+            if hour < 4:
+                timeFreqs['late_night'] += 1
+            elif hour >= 4 and hour < 7:
+                timeFreqs['early_morning'] += 1
+            elif hour >= 7 and hour < 12:
+                timeFreqs['morning'] += 1
+            elif hour >= 12 and hour < 17:
+                timeFreqs['afternoon'] += 1
+            elif hour >= 17:
+                timeFreqs['evening'] += 1 
+
+            # find the app the user prefers to tweet from
+            newSource = tweet.source
+            if newSource not in sources:
+                sources[newSource] = 1
+            else:
+                sources[newSource] += 1
+
+            # find the ratio of tweets with links to all tweets
 
 
-                # find the app the user prefers to tweet from
-                newSource = tweet.source
-                if newSource not in sources:
-                    sources[newSource] = 1
-                else:
-                    sources[newSource] += 1
-
-                # find the ratio of tweets with links to all tweets
-
-
-                # find the user's happiness on a scale of -1 to 1
-                # -1 = very unhappy, 1 = very happy
+            # find the user's happiness on a scale of -1 to 1
+            # -1 = very unhappy, 1 = very happy
 
         # add mostFavorited to JSON
         mostFavoritedInfo = {'id' : mostFavorited.id,
@@ -149,6 +170,13 @@ def analyzeUser(user):
                              'text' : mostRetweeted.text,
                              'retweet_count' : mostRetweeted.retweet_count}
         analysisInfo['mostRetweeted'] = mostRetweetedInfo
+
+        # analyze time frequencies to find most active time
+        mostActiveTime, most = "", 0
+        for activeTime, freq in timeFreqs.iteritems():
+            if freq > most:
+                mostActiveTime, most = activeTime, freq
+        analysisInfo['mostActiveTime'] = mostActiveTime
 
         # analyze sources list to find preferred source
         preferredSource, most = "", 0
