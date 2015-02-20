36c0b951adc5230a8ef94445880b110a54d45a58
Collin Barth <cpbarth92@gmail.com>
2015-02-20 01:27:05 -0500

    updated user list

--- creds.py
+++
@@ -1,7 +0,0 @@
-class Creds:
-    def __init__(self):
-        self.c_key = "V7Nw0tT3dps5qlo7ICoWE1uI6"
-        self.c_secret = "NHEVWdnpsagNvlpgWITdMCvmoXGSn6Hfkb2hYnfBXDH2laG9xd"
-
-        self.a_token = "613854165-OqM0QjS2rJsSA2Y0Cq1hnnp0BQ1FPQ7HPrKlEEEZ"
-        self.a_secret = "POi1vDKYE5z89IaZ1K4EjBBEJc5N9gR26wjkTcYi5B7VY"
